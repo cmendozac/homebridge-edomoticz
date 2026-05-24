0a39edc0b5feb2918e4ec36afb5047cc18c2d4f1
cmendozac <cmendozac@users.noreply.github.com>
2026-05-24 22:46:03 +0200

    refactor: drop util.inherits calls, invoke initServices factory
    Companion change to the lib/services.js ES6 migration. Removes the 34
    util.inherits(...) calls that previously wired Service/Characteristic
    inheritance after the fact -- with `class extends` they are now redundant.
    Adds a single initServices(Service, Characteristic, UUID) invocation that
    populates the eDomoticzServices object once homebridge.hap is available.
    
    The `util` require is kept: util.format is still used by the debug logger
    (this.log inside eDomoticzPlatform).

--- index.js
+++ index.js
@@ -14,7 +14,9 @@ var Mqtt = require('./lib/mqtt.js').Mqtt;
 var eDomoticzAccessory = require('./lib/domoticz_accessory.js');
 var Constants = require('./lib/constants.js');
 var Helper = require('./lib/helper.js').Helper;
-var eDomoticzServices = require('./lib/services.js').eDomoticzServices;
+var Services = require('./lib/services.js');
+var eDomoticzServices = Services.eDomoticzServices;
+var initServices = Services.initServices;
 const util = require('util');
 
 module.exports = function(homebridge) {
@@ -24,40 +26,11 @@ module.exports = function(homebridge) {
     Types = homebridge.hapLegacyTypes;
     UUID = homebridge.hap.uuid;
 
-    util.inherits(eDomoticzServices.TotalConsumption, Characteristic);
-    util.inherits(eDomoticzServices.CurrentConsumption, Characteristic);
-    util.inherits(eDomoticzServices.GasConsumption, Characteristic);
-    util.inherits(eDomoticzServices.TempOverride, Characteristic);
-    util.inherits(eDomoticzServices.MeterDeviceService, Service);
-    util.inherits(eDomoticzServices.GasDeviceService, Service);
-    util.inherits(eDomoticzServices.Ampere, Characteristic);
-    util.inherits(eDomoticzServices.AMPDeviceService, Service);
-    util.inherits(eDomoticzServices.Volt, Characteristic);
-    util.inherits(eDomoticzServices.VOLTDeviceService, Service);
-    util.inherits(eDomoticzServices.CurrentUsage, Characteristic);
-    util.inherits(eDomoticzServices.UsageDeviceService, Service);
-    util.inherits(eDomoticzServices.TodayConsumption, Characteristic);
-    util.inherits(eDomoticzServices.Barometer, Characteristic);
-    util.inherits(eDomoticzServices.WaterFlow, Characteristic);
-    util.inherits(eDomoticzServices.TotalWaterFlow, Characteristic);
-    util.inherits(eDomoticzServices.WaterDeviceService, Service);
-    util.inherits(eDomoticzServices.WeatherService, Service);
-    util.inherits(eDomoticzServices.WindSpeed, Characteristic);
-    util.inherits(eDomoticzServices.WindChill, Characteristic);
-    util.inherits(eDomoticzServices.WindDirection, Characteristic);
-    util.inherits(eDomoticzServices.WindDeviceService, Service);
-    util.inherits(eDomoticzServices.Rainfall, Characteristic);
-    util.inherits(eDomoticzServices.RainDeviceService, Service);
-    util.inherits(eDomoticzServices.Visibility, Characteristic);
-    util.inherits(eDomoticzServices.VisibilityDeviceService, Service);
-    util.inherits(eDomoticzServices.SolRad, Characteristic);
-    util.inherits(eDomoticzServices.SolRadDeviceService, Service);
-    util.inherits(eDomoticzServices.LocationService, Service);
-    util.inherits(eDomoticzServices.Location, Characteristic);
-    util.inherits(eDomoticzServices.InfotextDeviceService, Service);
-    util.inherits(eDomoticzServices.Infotext, Characteristic);
-    util.inherits(eDomoticzServices.UVDeviceService, Service);
-    util.inherits(eDomoticzServices.UVIndex, Characteristic);
+    // Populate eDomoticzServices with ES6 class definitions now that HAP refs
+    // (Service / Characteristic) are available. Replaces the previous pattern
+    // of pre-ES6 constructors + util.inherits, which crashes under HAP-NodeJS
+    // v2 ("Class constructor Service cannot be invoked without 'new'").
+    initServices(Service, Characteristic, UUID);
 
     //homebridge.registerAccessory("homebridge-edomoticz", "eDomoticz", eDomoticzAccessory);
     homebridge.registerPlatform("homebridge-edomoticz", "eDomoticz", eDomoticzPlatform, true);
